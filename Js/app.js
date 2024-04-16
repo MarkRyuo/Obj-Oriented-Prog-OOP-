e8a027d91b484ba713325b41093ec1609812a060
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-16 21:43:59 +0800

    update herE'

--- Js/app.js
+++ Js/app.js
@@ -3,15 +3,16 @@
 class Character {
 
     constructor(name, power, speed){
-        this.name = name
-        this.power = power
-        this.speed = speed 
+        this.name = name ;
+        this.power = power ;
+        this.speed = speed ;
     }
 
     introduction() {
         console.log(`Konichiwa ${this.name} desu`)
     }
 }
+
 
 const charOne = new Character("Nicole", "Moon Blade", "High diff") ;
 
