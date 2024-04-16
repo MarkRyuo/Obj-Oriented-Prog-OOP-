93b37f7845d420345e403783869aa7414d4411ae
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-16 10:07:29 +0800

    updta eherE

--- python/main.py
+++ python/main.py
@@ -1,12 +1,13 @@
 # OOP (Object Oriented Programming)
 
 class Character :
-    def __init__ (self, power, strength, speed) :
+    def __init__ (self, power, strength, speed) : #  it's like parameter 
         self.power = power 
         self.strength = strength
         self.speed = speed
 
-nicole = Character("Moon Slashes", 3000, "Midium")
-print(nicole)
+nicole = Character("Moon Slashes", 3000, "Midium") # it's argument 
+
+print(f"Character Nicole: {}")
 
     
