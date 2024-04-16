00b994e1d3e5abe79778b3f74b7d9e751ce4dbc9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-16 11:24:11 +0800

    udpate herE

--- Js/app.js
+++ Js/app.js
@@ -1,1 +1,9 @@
 // OOP (OBJECT ORIENTED PROGRAMMING)
+
+class Character {
+    constructor(self, name, power, speed){
+        self.name = name
+        self.power = power
+        self.speed = speed 
+    }
+}
