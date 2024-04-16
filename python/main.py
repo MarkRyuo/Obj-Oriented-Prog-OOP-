d7059fc1da3d471e53dacb67d6e49fc14a09eb83
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-16 10:04:07 +0800

    update herE

--- python/main.py
+++ python/main.py
@@ -1,8 +1,11 @@
 # OOP (Object Oriented Programming)
 
 class Character :
-    def __init__ (self, power, strength) :
+    def __init__ (self, power, strength, speed) :
         self.power = power 
         self.strength = strength
+        self.speed = speed
+
+nicole = Character("Moon Slashes", 3000, "Midium")
 
     
