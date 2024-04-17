48ff9b13e4425209872c40e6cb9112723da800a6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-17 10:31:46 +0800

    update herE

--- python/play.py
+++ python/play.py
@@ -13,8 +13,8 @@ class Person :
 
     
 
-person1 = Person("Mark", 21)
-person2 = Person("Nicole", 21)
+# person1 = Person("Mark", 21)
+# person2 = Person("Nicole", 21)
 
 person1.speak()
 person2.speak()
