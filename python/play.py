b5d595298b259586c58ef2b87dcc9170c608ea16
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-17 10:20:06 +0800

    udpate ehre

--- python/play.py
+++ python/play.py
@@ -5,7 +5,12 @@ class Person :
     def __init__(self, name, age) :
         self.name = name
         self.age = age
+
+    def speak (self) :
+        print(f"Hello Goodmorning")
+
     
 
 person1 = Person("Mark", 21)
 person2 = Person("Nicole", 21)
+
