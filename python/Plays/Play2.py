947cdacc1354f0d67d367fa64701d59693ecde2c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-18 22:02:53 +0800

    updta eherE

--- python/Plays/Play2.py
+++ python/Plays/Play2.py
@@ -1,8 +1,9 @@
 # Play 2 
 
 class Person : # Parent
-    def __init__(self, fname, age):
-        self.fname = name 
+    def __init__(self, fname, lname, age):
+        self.fname = fname 
+        self.lname = lname
         self.age = age
 
 class student(Person) : #Child
