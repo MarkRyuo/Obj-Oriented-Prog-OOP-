fc0ead2176f68fd15853f2578dee1f3b41853845
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-18 21:55:42 +0800

    update herE

--- python/Plays/Play2.py
+++ python/Plays/Play2.py
@@ -1,5 +1,12 @@
 # Play 2 
 
-class Person :
+class Person : # Parent
     pass
+
+class student(Person) :
+
+    def __init__(self, name, age, section):
+        self.name = name 
+        self.age = age
+        
 
