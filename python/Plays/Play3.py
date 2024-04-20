3b91726bba6f93f7521de6fc0af2b35335efe64e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-20 16:56:58 +0800

    updta eherE

--- python/Plays/Play3.py
+++ python/Plays/Play3.py
@@ -19,7 +19,7 @@ class Character(Person) : # Child
         self.fracta = fracta
 
     def speak(self) : 
-        print(f"Konichiwa {self.name} desu!")
+        print(f"Konichiwa {self.fname} desu!")
         print(f"Code name: {self.codename}")
 
-Character1 = Character(Char1[""])
+Character1 = Character(Char1["fname"], )
