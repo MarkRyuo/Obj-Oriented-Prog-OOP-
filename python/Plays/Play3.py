da52df2a1fc02c7e5d09e56da96cd84fe06f217c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-20 16:49:42 +0800

    udpat ehre

--- python/Plays/Play3.py
+++ python/Plays/Play3.py
@@ -5,11 +5,10 @@ import module
 Char1 = module.Char1 
 Char2 = module.Char2
 
-class Person :
+class Person : # Parent
 
     def __init__(self, fname, lname) :
         self.fname = fname
         self.lname = lname  
 
 class Character(Person) :
-    
