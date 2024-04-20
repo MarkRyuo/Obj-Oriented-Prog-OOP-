6b3951c097add232236f0a9e966a26be7be94f4a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-20 16:48:17 +0800

    update here

--- python/Plays/Play3.py
+++ python/Plays/Play3.py
@@ -1,10 +1,11 @@
 import module
 #OOP
 
+Char1 = module.Char1 
+Char2 = module.Char2
+
 class Person :
 
     def __init__(self, fname, lname) :
         self.fname = fname
-        self.lname = lname 
-    
-    
+        self.lname = lname  
