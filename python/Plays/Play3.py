9e7e6cce8f6dea348bb3dbeb454553a5685fd573
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-21 12:23:36 +0800

    updte ere

--- python/Plays/Play3.py
+++ python/Plays/Play3.py
@@ -5,7 +5,7 @@ import module
 Char1 = module.Char1  # declare a variable para sa laman ng module gumamit ng dot(.) notationg
 Char2 = module.Char2
 
-Char1.update({"lname" : "Malupa"}) ;
+Char1.update({"lname" : "Librea"}) ;
 
 class Person : # Parent
 
