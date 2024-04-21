7719c893f007619e7d7a64b73d6ce177a23f60e1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-21 15:09:18 +0800

    updat eherE

--- python/Plays/Play4.py
+++ python/Plays/Play4.py
@@ -5,16 +5,14 @@ import Klass
 Char1 = module.Char1
 Char2 = module.Char2 
 
-object = Klass.Character()
+Character = Klass.Character()
+Char1 = Character(Char1["fname"], Char1["lname"], Char1["age"])
 
 log = input("Enter your name: ")
 
 while not log :
     log = input("Enter your name: ")
-
-Char1 = Character(Char1["fname"], Char1["lname"], Char1["age"])
-
-    
+   
 if log == Char1["fname"] and log == Char2["fname"]:
     Char1.speak()  
 else :
