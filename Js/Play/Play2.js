2b67bcb0dc132df50aaed53d74f413d42fa74704
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-22 10:53:19 +0800

    updat egerE

--- Js/Play/Play2.js
+++ Js/Play/Play2.js
@@ -2,7 +2,9 @@
 
 class item {
 
-    constructor(barcode_number, product_name, quantity,){
+    constructor(barcode_number, product_name, quantity){
         this.barcode_number = barcode_number ;
+        this.product_name = product_name ;
+        this.quantity = quantity ;
     }
 }
