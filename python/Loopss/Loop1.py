2d2494dbb2461d073c401d252f8a71c808986fe2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-23 22:02:40 +0800

    updta eherRe

--- python/Loopss/Loop1.py
+++ python/Loopss/Loop1.py
@@ -6,4 +6,4 @@ _name = ["Jhon Mark", "Nicole", "Ryuo", "Samantya", "Sopheya", "Moda"]
 for count in range (0, 4) :
     print(_name[count])
 else :
-    print("Successful")
+    print("Successful Search")
