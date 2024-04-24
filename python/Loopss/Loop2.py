96edfb1f3fe288f155b1ed3b1694022b33a8c288
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-24 23:37:02 +0800

    update herE'

--- python/Loopss/Loop2.py
+++ python/Loopss/Loop2.py
@@ -4,4 +4,4 @@ listof_Name = ["Nicole", "Mark", "Ryuo", "Moda","Samantya","Sopheya"]
 for count in range(3, 5) :
     print(listof_Name[count])
 else :
-    print("Successfully") 
+    print("Successfully Print") 
