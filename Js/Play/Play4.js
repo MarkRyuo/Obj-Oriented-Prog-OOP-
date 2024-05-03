1cbac70e26d6618a2c1e348ad3810ec996d2a7ce
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-03 14:56:27 +0800

    updat eherE

--- Js/Play/Play4.js
+++ Js/Play/Play4.js
@@ -19,7 +19,7 @@ class Person {
     }
 
     talk() {
-        console.log(`Im ${this.fname} ${this.lname}, my age is ${this.age} years old`)
+        console.log(`Im ${this.fname} ${this.lname}, ${this.age} years old`)
     }
 }
 
