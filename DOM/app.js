453706e0f792dce85cf01d5dc743a310a6268006
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-04 21:04:52 +0800

    update herE

--- DOM/app.js
+++ DOM/app.js
@@ -1,6 +1,7 @@
 // DOM Document Object Model
 
 
+// Using ID 
 const element = document.getElementById("intro") ;
 
 document.getElementById("name").innerHTML = `Konichiwa Ryuo Desu ${element.innerHTML}` ;
