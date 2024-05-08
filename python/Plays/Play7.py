590a1a8b46f84fb27f2c4d4136d85d5a9710a377
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-08 21:25:31 +0800

    udpate herE

--- python/Plays/Play7.py
+++ python/Plays/Play7.py
@@ -17,7 +17,9 @@ _Char1 = module.Char1
 _Char2 = module.Char2
 
 def _Name() :
+    whi
     _person = input(f"Please choose a number ({CHO_NUM1} or {CHO_NOM2}): ")
     if _person.indigit() :
-        _person = int()
+        _person = int(_person)
+        if _perso
 
