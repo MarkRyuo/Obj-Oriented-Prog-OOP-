72131aeb5bdf7acba0af86f7ff72b2bf5803eb73
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-13 21:10:56 +0800

    update herE

--- python/Loopss/Loop5.py
+++ python/Loopss/Loop5.py
@@ -10,7 +10,8 @@ even_numbers = [
 
 def Loop() :
     input1 = input("Enter a number: ")
-
+    if input1.isdigit():
+        
     for i in range(even_numbers) :
         input1[i]
         print(input1)
