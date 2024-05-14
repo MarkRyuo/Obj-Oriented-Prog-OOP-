2f07294859fbac981db79da028de8e7e764c73b9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-14 17:15:25 +0800

    update here

--- python/Loopss/Loop5.py
+++ python/Loopss/Loop5.py
@@ -21,11 +21,8 @@ even_numbers = [
 
 def Loop() :
     input1 = input("Enter a number: ")
-    if input1.isdigit():
-        input1 = int(input1)
-        for i in range(even_numbers) :
-            input1[i]
-            print(input1)
+    
+    
 
 
 Loop()
