18069219447b6abcf0f2708f61bd5fd2f3983848
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-14 17:14:57 +0800

    update here

--- python/Loopss/Loop5.py
+++ python/Loopss/Loop5.py
@@ -18,3 +18,14 @@ def Loop() :
 
 
 Loop()
+
+def Loop() :
+    input1 = input("Enter a number: ")
+    if input1.isdigit():
+        input1 = int(input1)
+        for i in range(even_numbers) :
+            input1[i]
+            print(input1)
+
+
+Loop()
