c3d3cd67beb430e48c93dc56bde3a0a609e0ff75
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-16 19:27:48 +0800

    update here

--- OOP_IN_PY/Log.py
+++ OOP_IN_PY/Log.py
@@ -1,30 +1,6 @@
 
 
 
-class Log :
-
-    def __init__(self, _name, _age) :
-        self._name = _name  # * This called Attributes
-        self._age = _age 
-
-    def Username(self) :
-
-        user = True 
-
-        while user :
-
-            username = input("Enter your name: ")
-
-            if username :
-                print("hello")
-                break 
-            else :
-                print("LOL enter a name")
-
-
-
-
-class Main :
 
      
     
