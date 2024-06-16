86d5166bb545fd0ee815072ae37499f6d6afbf18
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-16 19:33:18 +0800

    update here

--- OOP_IN_PY/Log.py
+++ OOP_IN_PY/Log.py
@@ -5,10 +5,18 @@ class Student :
     def __init__(self, name, age, grade) :
         self.name = name 
         self.age = age 
-        self.grade = grade # 
+        self.grade = grade # * 0 - 100 
     
     def get_grade(self) :
         return self.grade
+    
+
+class course :
+
+    def __init__(self, course_name, max_student) :
+        self.course_name = course_name 
+        self.max_students = max
+
      
     
 
