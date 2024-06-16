8ca9524dcf77ad7939de2d97ceedf0a2df45bbcf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-16 19:23:31 +0800

    update here

--- OOP_IN_PY/Log.py
+++ OOP_IN_PY/Log.py
@@ -7,14 +7,7 @@ class Log :
         self._name = _name  # * This called Attributes
         self._age = _age 
 
-    def Log_in(self) :
-
-        username = input("Enter your name: ") # ! Fix This 
-
-        if username :
-            print(f"Hi {self._name}")
-        else :
-            pass
+   def 
 
 
 
